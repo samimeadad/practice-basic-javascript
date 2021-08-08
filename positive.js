ccb88a25b70f37ce3baf5c65aebc35297f2cbed9
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-08 20:03:55 +0600

    all practice done Alhamdulillah

--- positive.js
+++ positive.js
@@ -11,7 +11,7 @@ function findPositiveNumber(arr){
     return positiveArray;
 }
 
-let number = [1, 5, -2, 6, 3, 8, -7, 9, 4, 10, -4];
+let number = [1, 5, 2, 6, 3, 8, -7, 9, 4, 10, -4];
 let positiveNumber = findPositiveNumber(number);
 console.log(number);
 console.log(positiveNumber);
